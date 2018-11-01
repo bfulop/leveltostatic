2f9818f1bf7539bb8968a03aeb3840d4babccc29
Balint Fulop <bfulop@mac.com>
2018-11-01 18:03:28 +0100

    index.js skeleton seems OK

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 const { task } = require('folktale/concurrency/task')
-const db = require('./getdb')
+const db = require('./getdb')()
 const to = require('to2')
+const createNotePage = require('./createNotePage')
 
 const write = (buf, enc, next) => {
-  console.log('buf', buf.toString())
-  console.log('%%%%%%%%%%%%%%%%%%%%%')
-  next()
+  console.log(buf.toString())
+  db.get(buf.toString(), (e, v) => {
+    createNotePage({
+      notedata: v.toString(),
+    })
+    next()
+  })
 }
 const end = r => next => {
-  console.log('end')
   r.resolve('end')
   next()
 }
 
-const runme = () => task(r => db().createKeyStream().pipe(to(write, end(r))))
+const runme = () =>
+  task(r =>
+    db
+      .createKeyStream({
+        gt: 'note:',
+        lt: 'note:~',
+      })
+      .pipe(to(write, end(r))),
+  )
 
 module.exports = runme
--- src/index.test.js
+++ src/index.test.js
@@ -2,21 +2,53 @@ const levelup = require('levelup')
 const memdown = require('memdown')
 
 const db = levelup(memdown())
-db.put('hey', 'you')
 
 jest.mock('./getdb')
 const getDB = require('./getdb')
 getDB.mockReturnValue(db)
 
+jest.mock('./createNotePage')
+const createNotePage = require('./createNotePage')
+
 const subject = require('./index')
 
-test('running subject', done => {
-  subject()
-  .run()
-  .listen({
-    onResolved: t => {
-      expect(t).toEqual('end')
-      done()
-    }
+describe('walking through the db', () => {
+  beforeAll(done => {
+    db.batch()
+      .put('note:aaa1', 'note_aaa1_data')
+      .put('note:aaa2', 'note_aaa2_data')
+      .put('note:aaa3', 'note_aaa3_data')
+      .put('note:baa1', 'note_baa1_data')
+      .put('note:baa2', 'note_baa2_data')
+      .put('note:baa3', 'note_baa3_data')
+      .put('anotebook:aa:100:aaa1')
+      .put('anotebook:aa:101:aaa2')
+      .put('anotebook:aa:102:aaa3')
+      .put('anotebook:ba:103:baa1')
+      .put('anotebook:ba:104:baa2')
+      .put('anotebook:ba:105:baa3')
+      .put('notebooks:100:aa')
+      .put('notebooks:103:ba')
+      .write(() => {
+        done()
+      })
+  })
+  test('running subject', done => {
+    subject()
+      .run()
+      .listen({
+        onResolved: t => {
+          expect(t).toEqual('end')
+          done()
+        },
+      })
+  })
+  test('calls a creaateNotePage', done => {
+    expect(createNotePage.mock.calls[0][0]).toEqual({
+      notedata: 'note_aaa1_data',
+      siblings: ['aaa1', 'aaa2', 'aaa3'],
+      notebooks: ['aa', 'ba'],
+    })
+    done()
   })
 })
