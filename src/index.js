87370d59aaea458a6d47e72e9618e53cb6ec8fe8
Balint Fulop <bfulop@mac.com>
2018-11-02 20:25:41 +0100

    basics working (control flow, passing data)

--- src/index.js
+++ src/index.js
@@ -2,30 +2,29 @@ const { task, of } = require('folktale/concurrency/task')
 const R = require('ramda')
 const db = require('./getdb')()
 const to = require('to2')
+const through = require('through2')
 const createNotePage = require('./createNotePage')
 
 const siblingNotes = () => of(['aaa1', 'aaa2', 'aaa3'])
 const noteBooks = () => of(['aa', 'ba'])
-const noteData = () => of('note_aaa1_data')
-
-const collectParents = r => siblingNotes(r).and(noteBooks())
+const collectParents = R.compose(
+  e => siblingNotes(e).and(noteBooks()),
+  R.path(['meta', 'notebook'])
+)
 
 const processNote = (buf, enc, next) => {
-  noteData(buf.toString)
-    .chain(r =>
-      R.compose(
-        R.map(R.zipObj(['siblings', 'notebooks', 'notedata'])),
-        R.map(R.append(r)),
-        collectParents
-      )(r)
-    )
-    .run()
-    .listen({
-      onResolved: r => {
-        next()
-        return createNotePage(r)
-      }
-    })
+  return R.compose(
+    r =>
+      r.run().listen({
+        onResolved: r => {
+          next()
+          return createNotePage(r)
+        }
+      }),
+    R.map(R.zipObj(['siblings', 'notebooks', 'notedata'])),
+    R.map(R.append(buf)),
+    collectParents
+  )(buf)
 }
 
 const end = r => next => {
@@ -36,11 +35,12 @@ const end = r => next => {
 const runme = () =>
   task(r =>
     db
-      .createKeyStream({
+      .createValueStream({
+        valueAsBuffer: false,
         gt: 'note:',
         lt: 'note:~'
       })
-      .pipe(to(processNote, end(r)))
+      .pipe(through.obj({ objectMode: true }, processNote, end(r)))
   )
 
 module.exports = runme
